135cd434f1c5993e2ec5527e412c199c3b83639a
Satyam Kumar Navneet <76639713+Satyamkumarnavneet@users.noreply.github.com>
2024-01-10 00:31:00 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0519-random-flip-matrix/0519-random-flip-matrix.java
+++ 0519-random-flip-matrix/0519-random-flip-matrix.java
@@ -1,39 +1,36 @@
 class Solution {
-    private int m, n;
-    private int totalCells;
-    private Map<Integer, Integer> indexMap;
-    private Random random;
+    private int currentRow = 0;
+    private int currentCol = 0;
+    private int totalRows = 0;
+    private int totalCols = 0;
+    private boolean matrixFlipped = false;
 
-    public Solution(int m, int n) {
-        this.m = m;
-        this.n = n;
-        this.totalCells = m * n;
-        this.indexMap = new HashMap<>();
-        this.random = new Random();
-        reset();
+    public Solution(int totalRows, int totalCols) {
+        this.totalRows = totalRows;
+        this.totalCols = totalCols;
     }
 
     public int[] flip() {
-        // Generate a random number between 0 (inclusive) and totalCells (exclusive)
-        int rand = random.nextInt(totalCells--);
+        if (matrixFlipped || currentRow == totalRows) {
+            return null;  // All cells are flipped or matrix has been reset
+        }
 
-        // Use the generated random number to get the corresponding index from the map
-        int index = indexMap.getOrDefault(rand, rand);
+        int[] result = new int[]{currentRow, currentCol};
+        currentCol++;
 
-        // Update the map to reflect the current state after flipping
-        indexMap.put(rand, indexMap.getOrDefault(totalCells, totalCells));
+        if (currentCol == totalCols) {
+            currentCol = 0;
+            currentRow++;
+        }
 
-        // Convert the index to [row, col] format
-        int row = index / n;
-        int col = index % n;
+        if (currentRow == totalRows) {
+            matrixFlipped = true;
+        }
 
-        // Return the result
-        return new int[]{row, col};
+        return result;
     }
 
     public void reset() {
-        // Reset the map and totalCells to their initial state
-        indexMap.clear();
-        totalCells = m * n;
+        
     }
 }
