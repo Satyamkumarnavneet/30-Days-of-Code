b88021d27b810cd681bbdd29a3eb38745e9390b5
Satyam Kumar Navneet <76639713+Satyamkumarnavneet@users.noreply.github.com>
2024-02-03 22:00:47 +0530

    Time: 11 ms (58.07%), Space: 44.5 MB (79.03%) - LeetHub

--- 0532-k-diff-pairs-in-an-array/0532-k-diff-pairs-in-an-array.java
+++ 0532-k-diff-pairs-in-an-array/0532-k-diff-pairs-in-an-array.java
@@ -1,31 +1,21 @@
+import java.util.HashMap;
+import java.util.Map;
+
 class Solution {
     public int findPairs(int[] nums, int k) {
-        int i=0;
-        int j=1;
-        int m=0;
-        int abs=0;
-        Arrays.sort(nums);
-        int sum=Integer.MIN_VALUE;
-        while(i<nums.length-1 && j<nums.length){
-            //abs=Math.abs(nums[i]-nums[j]);
-            //System.out.println(abs);
-            if( Math.abs(nums[i]-nums[j])==k && (nums[i]+nums[j])!=sum){
-                  
-                     sum=nums[i]+nums[j];
-                       m++;  
-                    i++; j++;      
+        if (k < 0) {
+            return 0;
+        }
+        Map<Integer, Integer> map = new HashMap<>();
+        int count = 0;
+        for (int num : nums) {
+            map.put(num, map.getOrDefault(num, 0) + 1);
+        }
+        for (int key : map.keySet()) {
+            if ((k == 0 && map.get(key) > 1) || (k != 0 && map.containsKey(key + k))) {
+                count++;
             }
-            else if((nums[j]-nums[i])<k){
-                j++;
-            }
-			//else case, when diff is greater than k, increase i by 1
-			else{
-                i++;
-            }
-			//check if i and j are not same to aoid duplicates
-            if(i==j) j++;
-            }
-        
-        return m;
+        }
+        return count;
     }
 }
