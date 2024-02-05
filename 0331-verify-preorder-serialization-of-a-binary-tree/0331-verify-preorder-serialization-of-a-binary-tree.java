22f58eafc381499f60fe79f4d991cf2e4e5e1afc
Satyam Kumar Navneet <76639713+Satyamkumarnavneet@users.noreply.github.com>
2024-02-05 23:39:44 +0530

    Time: 7 ms (33.03%), Space: 43.4 MB (72.08%) - LeetHub

--- 0331-verify-preorder-serialization-of-a-binary-tree/0331-verify-preorder-serialization-of-a-binary-tree.java
+++ 0331-verify-preorder-serialization-of-a-binary-tree/0331-verify-preorder-serialization-of-a-binary-tree.java
@@ -1,12 +1,19 @@
 class Solution {
     public boolean isValidSerialization(String preorder) {
-    String[] nodes = preorder.split(",");
-    int diff = 1;
-    for (String node: nodes) {
-        if (--diff < 0) return false;
-        if (!node.equals("#")) diff += 2;
-    }
-    return diff == 0;
-}
-}
+        String[] nodes = preorder.split(",");
+        Stack<String> stack = new Stack<>();
 
+        for (String node : nodes) {
+            while (node.equals("#") && !stack.isEmpty() && stack.peek().equals("#")) {
+                stack.pop(); // Pop two consecutive '#' and their parent node
+                if (stack.isEmpty()) {
+                    return false; // Invalid serialization
+                }
+                stack.pop();
+            }
+            stack.push(node); // Push the current node onto the stack
+        }
+
+        return stack.size() == 1 && stack.peek().equals("#");
+    }
+}
